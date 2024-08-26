9dd30e48b18ad992257f306c3f77295492267df4
Jeremy Duwe <jerine89@gmail.com>
2024-08-26 20:14:58 +0300

    header main footer

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react';
+import Header from '/Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+};
+
+
+
 
 function App() {
   return (
